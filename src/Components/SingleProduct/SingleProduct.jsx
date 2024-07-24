f205b6f41dcead6e868c6d5730d08a05e5d9b50a
Nirvik Roy <nirvikroy802@gmail.com>
2024-07-24 16:33:31 +0530

    product description done

--- src/Components/SingleProduct/SingleProduct.jsx
+++ src/Components/SingleProduct/SingleProduct.jsx
@@ -6,8 +6,11 @@ import './SingleProduct.scss'
 import shoe from '../../assets/sports-shoe1-600x600.jpg'
 import { useDispatch, useSelector } from 'react-redux'
 import { addElement } from '../../Store/Slices/Slices'
+import Footer from '../Footer/Footer.jsx'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const SingleProduct = () => {
-
+const data2 = API_DATA
   const data=useSelector((state)=>{ //Very important steps//
     return state.cart
   })  
@@ -29,7 +32,9 @@ const SingleProduct = () => {
   setproduct(filter_data)
  }
  useEffect(()=>{
+  window.scrollTo({top:0,behavior:'instant'})
   Single_product()
+
  },[])
  const setIndexFun =(i)=>{
   setIndex(i)
@@ -52,10 +57,33 @@ const SingleProduct = () => {
     image:cartitem.image
    }
    dispatch(addElement(obj))
-   console.log(cartitem.image)
+   
+   toast.info('Item Added To Cart', {
+    position: "top-left",
+    autoClose: 1500,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+    
+    });
  }
   return (
     <>
+    <ToastContainer
+position="top-left"
+autoClose={1500}
+hideProgressBar={false}
+newestOnTop={false}
+closeOnClick
+rtl={false}
+pauseOnFocusLoss
+draggable
+pauseOnHover
+theme="light"
+/>
      <Navbar2/>
      <section className='singleProduct_section'>
       <div className='single_product_main'>
@@ -84,8 +112,8 @@ const SingleProduct = () => {
       <div className='singleproduct_details_div'>
         <a>Home/</a><a>Men/</a><a>{e.title}</a>
         <p className='mt-4 text-uppercase'>{e.category}</p>
-        <h1>{e.title}</h1>
-        <h2 className='d-inline'>${e.price}</h2><span className='fs-5'> +Free Shipping</span>
+        <h2 className='mb-3'>{e.title}</h2>
+        <h4 className='d-inline'>${e.price}</h4><span > +Free Shipping</span>
         <p className='mt-3 product_description'>Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris in erat justo. Nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non neque elit sed.</p>
         <div className='color_div_main'>
         {e.color.map((ele)=>(
@@ -110,10 +138,87 @@ const SingleProduct = () => {
         </div>
        
       </div>
+      
       <div className='d-flex justify-content-center'>
       <hr className='hr_line'/>
+      
       </div>
+      <div className='d-flex justify-content-center'>
+      <div className='description_review_div'>
+      <div className=' description_review_main d-flex justify-content-between'>
+        <p><strong>Description</strong></p>
+        <p><strong>Reviews(0)</strong></p>
+      </div>
+      </div>
+</div>
+    <div className='d-flex justify-content-center'>
+      <div className='mt-4 product_description_wrapper'>
+        <h3 className='mt-4'>Product Description</h3>
+        <p className='mt-4'>Since it’s creation lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p className='mt-5'>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.</p>
+        <h3 className='mt-5'>More about the product</h3>
+        <p className='mt-4'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in.</p>
+        <p className='mt-5'>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.</p>
+      </div>
+     
+    </div>
+    <div className='d-flex justify-content-center mt-5'>
+    <div className='review_div_wrapper mt-1 d-flex justify-content-center'>
+      <div className='review_form_div'>
+        <h5 className='mt-5'>Your email address will not be published. Required fields are marked *</h5>
+        <p className='d-inline fs-5'>Your Rating* </p><i className="fa-regular fa-star star2 mt-4"></i>
+                   <i className="fa-regular fa-star star2"></i>
+                   <i className="fa-regular fa-star star2"></i>
+                   <i className="fa-regular fa-star star2"></i>
+                   <i className="fa-regular fa-star star2"></i>
+               <form>
+                <label className='fs-5 mt-4'>Your Review*</label>
+                <input type='text' className='review_input mt-3'></input>
+                <label className='d-block fs-5 mt-4'>Your Name*</label>
+                <input type='text' className='review_name_input mt-3'></input>
+                <label className='d-block fs-5 mt-4'>Your Email*</label>
+                <input type='email' className='review_email_input mt-3'></input>
+                <button type='submit' className='submit_review_btn'>SUBMIT</button>
+               </form>    
+      </div>
+    </div>
+    </div>
+
+    <div className='realted_products_main d-flex justify-content-center'>
+      <div className='realted_products_wrapper'>
+       <h1>Related Products</h1>
+       <div className='realted_products_divs'>
+
+       {data2.map((e,index)=>{
+        if(index>16&&index<=19){
+          return<>
+        
+        <div className='realted_product'>
+
+<div className='realted_product_img_div'>
+  <img className='realted_product_img' src={e.image}></img>
+</div>
+<h5 className='mt-3'>{e.title}</h5>
+<small>{e.category}</small>
+<p>${e.price}</p>
+<i className="fa-regular fa-star"></i>
+               <i className="fa-regular fa-star"></i>
+               <i className="fa-regular fa-star"></i>
+               <i className="fa-regular fa-star"></i>
+</div>
+        </>
+        }
+        
+       })}
+
+
+   
+
+       </div>
+      </div>
+    </div>
      </section>
+     <Footer/>
     </>
   )
 }
